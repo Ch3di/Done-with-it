3a446681cd5e6b278bd26c7d13392eb1197d1974
Chady <25162159+Ch3di@users.noreply.github.com>
2020-09-13 21:53:11 +0100

    implemented AppPicker component

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import Screen from "./app/components/Screen";
 import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
+
+const categories = [
+    { label: "Furniture", value: 1 },
+    { label: "Clothing", value: 2 },
+    { label: "Cameras", value: 3 }
+];
+
 export default function App() {
+    const [category, setCategory] = useState(categories[0]);
     return (
         <Screen>
             <AppTextInput icon="email" />
+            <AppPicker
+                selectedItem={category}
+                onSelectItem={(item) => setCategory(item)}
+                icon="apps"
+                placeholder="categories"
+                items={categories}
+            />
         </Screen>
     );
 }
